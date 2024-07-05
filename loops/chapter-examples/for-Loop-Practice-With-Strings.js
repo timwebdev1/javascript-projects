6a563a9c9f43b64779d690b6513e69ac885fb65f
Timothy McDermott <tlmcdermott79@gmail.com>
2024-07-05 11:23:48 -0500

    completed chapter exercise

--- loops/chapter-examples/for-Loop-Practice-With-Strings.js
+++ loops/chapter-examples/for-Loop-Practice-With-Strings.js
@@ -1,4 +1,8 @@
 // Create a string variable containing your name.
+let myNameString = "Timothy McDermott";
 
 
 // Write a for loop that prints each character in your name on a different line.
+for (let i = 0; i < myNameString.length; i++) {
+  console.log(myNameString[i]);
+}
