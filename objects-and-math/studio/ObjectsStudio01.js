b7dd56911b7cb5945dc558fcca68d0904531c8e1
Timothy McDermott <tlmcdermott79@gmail.com>
2024-07-08 21:43:59 -0500

    completed object/math studio

--- objects-and-math/studio/ObjectsStudio01.js
+++ objects-and-math/studio/ObjectsStudio01.js
@@ -1,8 +1,31 @@
 // Code your selectRandomEntry function here:
 
+function selectRandomEntry(array){
+  let randomIndex = Math.floor(Math.random() * array.length);
+  return array[randomIndex];
+}
+
+function getThreeIds(array) {
+  let newArray = [];
+  while (newArray.length < 3) {
+    let selectedItem = selectRandomEntry(array);
+    if (newArray.includes(selectedItem) === false) {
+      newArray.push(selectedItem);
+    }
+  }
+  return newArray;
+}
 
 // Code your buildCrewArray function here:
-
+function buildCrewArray(array1, array2){
+  let crew = [];
+  for (let candidate in array2) {
+    if (array1.includes(array2[candidate].astronautID) === true) {
+      crew.push(array2[candidate].name);
+    }
+  }
+  return crew;
+}
 
 let idNumbers = [291, 414, 503, 599, 796, 890];
 
@@ -53,3 +76,6 @@ let candidateF = {
 let animals = [candidateA,candidateB,candidateC,candidateD,candidateE,candidateF];
 
 // Code your template literal and console.log statements:
+
+let selectedCrew = buildCrewArray(getThreeIds(idNumbers), animals);
+console.log(`${selectedCrew[0]}, ${selectedCrew[1]}, and ${selectedCrew[2]} are going to space!`)
--- objects-and-math/studio/ObjectsStudio02.js
+++ objects-and-math/studio/ObjectsStudio02.js
@@ -1,13 +1,32 @@
 // Code your orbitCircumference function here:
 
+let altitude = 2000;
+let orbitalSpeed = 28000;
+let orbitsComplete = 5;
+
+function orbitCircumference(orbit) {
+  return Math.round(2 * Math.PI * (orbit + 6378));
+}
 
 // Code your missionDuration function here:
-
+function missionDuration(orbitsComplete, orbitCircumfrence, orbitSpeed) {
+  return Math.round(((orbitsComplete * orbitCircumfrence) / orbitSpeed) * 1000) / 1000;
+}
 
 // Copy/paste your selectRandomEntry function here:
-
+function selectRandomEntry(array){
+  let randomIndex = Math.floor(Math.random() * array.length);
+  return array[randomIndex];
+}
 
 // Code your oxygenExpended function here:
+function oxygenExpended () {
+  let hours = missionDuration(3, orbitCircumference(altitude), orbitalSpeed);
+  let crewMember = selectRandomEntry(crew);
+  let oxygenWeight = Math.round(crewMember.o2Used(hours) * 10000) / 10000;
+
+  return [crewMember.name, hours, oxygenWeight];
+}
 
 
 // Candidate data & crew array.
@@ -54,5 +73,12 @@ let candidateA = {
    'astronautID':890
  };
  
- let crew = [candidateA,candidateC,candidateE];
- 
+let crew = [candidateA,candidateC,candidateE];
+
+let circumfrence = orbitCircumference(altitude);
+let duration = missionDuration(orbitsComplete, circumfrence, orbitalSpeed);
+console.log(`The mission will travel ${orbitsComplete * circumfrence} km around the planet, and it will take ${duration} hours to complete.`);
+
+let finalResult = oxygenExpended();
+
+console.log(`${finalResult[0]} will perform the spacewalk, which will last ${finalResult[1]} hours and require ${finalResult[2]} kg of oxygen.`)
