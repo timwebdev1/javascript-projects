6f0a9ad9a0b1d26cd985543f74fc7bb26389f1ed
Timothy McDermott <tlmcdermott79@gmail.com>
2024-07-15 20:20:54 -0500

    completed unit-test studio

--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -1,8 +1,56 @@
 // launchcode.test.js code:
 const launchcode = require('../index.js');
 
-describe("Testing launchcode", function(){
+describe("Testing launchcode", function () {
 
-  // Write your unit tests here!
-  
+  test("should return nonprofit", function () {
+    expect(launchcode.organization).toBe('nonprofit');
+  });
+
+  test("should return Jeff", function () {
+    expect(launchcode.executiveDirector).toBe('Jeff');
+  });
+
+  test("should return 100", function () {
+    expect(launchcode.percentageCoolEmployees).toBe(100);
+  });
+
+  test("should return array of programs", function () {
+    expect(launchcode.programsOffered).toContain("Web Development");
+    expect(launchcode.programsOffered).toContain("Data Analysis");
+    expect(launchcode.programsOffered).toContain("Liftoff");
+    expect(launchcode.programsOffered).toHaveLength(3);
+  });
+
+  test("should return 'Launch!' when only divisible by 2", function () {
+    expect(launchcode.launchOutPut(4)).toBe("Launch!");
+  });
+
+  test("should return 'Code!' when only divisible by 3", function () {
+    expect(launchcode.launchOutPut(3)).toBe("Code!");
+  });
+
+  test("should return 'Rocks!' when only divisible by 5", function () {
+    expect(launchcode.launchOutPut(5)).toBe("Rocks!");
+  });
+
+  test("should return 'LaunchCode!' when divisible by 2 and 3", function () {
+    expect(launchcode.launchOutPut(6)).toBe(("LaunchCode!"));
+  });
+
+  test("should return 'Code Rocks! when divisible by 3 and 5", function () {
+    expect(launchcode.launchOutPut(15)).toBe("Code Rocks!");
+  });
+
+  test("should return 'Launch Rocks! (CRASH!!!!)' when divisible by 2 and 5", function () {
+    expect(launchcode.launchOutPut(10)).toBe('Launch Rocks! (CRASH!!!!)');
+  });
+
+  test("should return 'LaunchCode Rocks! when divisible by 2, 3 and 5", function () {
+    expect(launchcode.launchOutPut(30)).toBe("LaunchCode Rocks!");
+  });
+
+  test("should return 'Rutabagas!' That doesn't work.' when not divisible by 2, 3 and 5", function () {
+    expect(launchcode.launchOutPut(49)).toBe('Rutabagas! That doesn\'t work.');
+  });
 });
