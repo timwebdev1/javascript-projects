4d2974d082f208a1bfba82a3c695d2a1992eb7fc
Timothy McDermott <tlmcdermott79@gmail.com>
2024-07-18 21:06:17 -0500

    completed

--- exceptions/exercises/divide.js
+++ exceptions/exercises/divide.js
@@ -1,5 +1,18 @@
 // Write a function called 'divide' that takes two parameters: a numerator and a denominator.
+let a = 10;
+let b = 0;
 
+function divide(numerator, denominator){
+  if (denominator === 0) {
+    throw Error('Attempted to divide by zero.');
+ 
+  } else {
+    return numerator/denominator;
+  }
+  
+}
+
+console.log(divide(a, b));
 // Your function should return the result of numerator / denominator.
 
 // However, if the denominator is zero you should throw the error, "Attempted to divide by zero."
--- exceptions/exercises/test-student-labs.js
+++ exceptions/exercises/test-student-labs.js
@@ -1,24 +1,55 @@
 function gradeLabs(labs) {
   for (let i=0; i < labs.length; i++) {
-    let lab = labs[i];
-    let result = lab.runLab(3);
-    console.log(`${lab.student} code worked: ${result === 27}`);
+      let lab = labs[i];
+      let result;
+
+      try{
+        result = lab.run.Lab(3);
+        console.log(`${lab.student} code worked: ${result === 27}`);
+      }
+      catch(error){
+        result = "Error thrown";
+        console.log(result);
+      }
   }
 }
 
 let studentLabs = [
   {
-    student: 'Carly',
-    runLab: function (num) {
+      student: 'Carly',
+      runLab: function (num) {
         return Math.pow(num, num);
-    }
+      }
   },
   {
-    student: 'Erica',
-    runLab: function (num) {
+      student: 'Erica',
+      runLab: function (num) {
         return num * num;
-    }
+      }
   }
 ];
 
 gradeLabs(studentLabs);
+
+let studentLabs2 = [
+  {
+      student: 'Blake',
+      myCode: function (num) {
+        return Math.pow(num, num);
+      }
+  },
+  {
+      student: 'Jessica',
+      runLab: function (num) {
+        return Math.pow(num, num);
+      }
+  },
+  {
+      student: 'Mya',
+      runLab: function (num) {
+        return num * num;
+      }
+  }
+];
+
+gradeLabs(studentLabs2);
