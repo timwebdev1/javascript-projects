c3343b43ea48b036df3ae0295883a0505936e5a2
Timothy McDermott <tlmcdermott79@gmail.com>
2024-08-06 17:35:02 -0500

    completed DOM exercise

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -1,10 +1,31 @@
-function init () {
-    const missionAbort = document.getElementById("abortMission");
-    const button = document.getElementById("liftoffButton");
-    const paragraph = document.getElementById("statusReport");
+function init() {
+  const missionAbort = document.getElementById("abortMission");
+  const button = document.getElementById("liftoffButton");
+  const paragraph = document.getElementById("statusReport");
 
-    // Put your code for the exercises here.
-    
-}
+  // Put your code for the exercises here.
+  button.addEventListener("click", function () {
+    paragraph.innerHTML = "Houston, we have liftoff!";
+  });
+
+  missionAbort.addEventListener("mouseover", function (event) {
+    const element = event.target;
+    element.style.backgroundColor = "red";
+  });
+
+  missionAbort.addEventListener("mouseout", function (event) {
+    event.target.style.backgroundColor = "";
+  });
+
+  missionAbort.addEventListener("click", function() {
+    const confirmed = window.confirm("Are you sure you want to abort the mission?");
+    if (confirmed) {
+      paragraph.innerHTML = "Mission aborted! Space shuttle returning home";
+    } else {
+        paragraph.innerHTML = "Mission continues. Stay on course!";
+    }
+  });
+
+};
 
 window.addEventListener("load", init);
