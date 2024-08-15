dbc1afb4fe0d5ba17614ec95d0e2865dd6af0bb4
Timothy McDermott <tlmcdermott79@gmail.com>
2024-08-15 12:48:28 -0500

    completed DOM exercises

--- user-input-with-forms/exercises/script.js
+++ user-input-with-forms/exercises/script.js
@@ -1,1 +1,14 @@
 //Code Your Solution Below
+window.addEventListener("load", function() {
+  let form = document.querySelector("form");
+  form.addEventListener("submit", function(event) {
+    let testNameInput = document.querySelector("input[name=testName]");
+    let testDateInput = document.querySelector("input[name=testDate]");
+    if (testNameInput.value === "" || testDateInput.value === "") {
+      alert("All fields are required!");
+
+      event.preventDefault();
+    }
+  });
+});
+
